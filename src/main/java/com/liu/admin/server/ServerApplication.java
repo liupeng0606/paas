44def5e14761227c88fa0b7957c426cac1fc64e1
liupeng <liupeng@hist.edu.cn>
2019-03-17 12:36:47 +0800

    Update ServerApplication.java

--- src/main/java/com/liu/admin/server/ServerApplication.java
+++ src/main/java/com/liu/admin/server/ServerApplication.java
@@ -2,6 +2,8 @@ package com.liu.admin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * 启动类
@@ -11,10 +13,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *         2017年4月18日
  */
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer{
+	
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ServerApplication.class);
+    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
 }
+
