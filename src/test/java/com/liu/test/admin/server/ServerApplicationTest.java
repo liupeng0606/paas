71f12e67d052e07245120ec218cd02b99fbc0c56
liupeng <liupeng@hist.edu.cn>
2019-06-10 20:57:30 +0800

    标准化文档，update启动类

--- src/test/java/com/liu/test/admin/server/ServerApplicationTest.java
+++ src/test/java/com/liu/test/admin/server/ServerApplicationTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = ServerApplicationTest.class)
 public class ServerApplicationTest {
 
+	
 	@Test
 	public void test1() {
 		
